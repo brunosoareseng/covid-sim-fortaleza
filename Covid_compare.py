510b8de933768f3111be6a8edec43fef1454b5d2
Bruno Soares <brunolucassoares@gmail.com>
2020-04-25 13:40:23 -0300

    Incluido Covid_compare para ler dados da simulação e comparar com dados reais

---
+++ Covid_compare.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Lê dados reais
+result = pd.read_csv('C:/Users/Bruno/PycharmProjects/COVID_Simulation/arquivos/CovidCE.csv', sep=';')
+
+# Lê dados da simulação
+Simulacao = pd.read_csv('C:/Users/Bruno/PycharmProjects/COVID_Simulation/arquivos/SimulacaoCovidCE_2020.04.22.csv', decimal=",", sep=";")
+
+# Simulacao = {'dias': t, 'Infectados': I, 'Obitos': Ob, 'Confirmados': I+R}
+print(Simulacao)
+
+# Plota dados reais
+plt.plot(result.values.transpose()[0], result.values.transpose()[1], label='Real_Casos', linewidth=4.0)
+plt.plot(result.values.transpose()[0], result.values.transpose()[2], label='Real_Obitos', linewidth=4.0)
+
+print(Simulacao.values.transpose()[0])
+
+# Plota dados da simulação
+plt.plot(Simulacao.values.transpose()[0], Simulacao.values.transpose()[1], label='SIM_Infectados', linewidth=1.0)
+plt.plot(Simulacao.values.transpose()[0], Simulacao.values.transpose()[2], label='SIM_Obitos', linewidth=1.0)
+plt.plot(Simulacao.values.transpose()[0], Simulacao.values.transpose()[3], label='SIM_Casos', linewidth=1.0)
+
+plt.show()
